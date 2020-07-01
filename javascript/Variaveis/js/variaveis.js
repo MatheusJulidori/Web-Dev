76543c3646cc5121aa75d272f33376a3dd9990b5
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-07-01 15:56:34 -0300

    aula 151

---
+++ javascript/Variaveis/js/variaveis.js
@@ -0,0 +1,19 @@
+//JavaScript sabe automaticamente que tipo de variavel é
+
+
+//string
+var texto = 'Hello World'
+var teste = '7'
+
+//number
+var numeroInt = 7
+var numeroFloat = -9.7
+
+//boolean
+var estado = true
+
+//Comandos de debug
+
+//alert() -->Abre um pop-up cujo texto é o valor em parentese
+//document.write() --> Escreve na pagina html
+//console.log --> Printa no console

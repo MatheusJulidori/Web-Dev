1efbe663ad547f8654e70392884fed9050784008
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-07-01 16:15:10 -0300

    aula 154

---
+++ javascript/NullvsUndefined/js/variaveis.js
@@ -0,0 +1,11 @@
+//null
+var teste = null
+/*Representa intencionalmente que não há valor*/
+
+//undefined
+var teste2
+
+/*Valor inexistente, nunca foi definido*/
+
+document.writeln(teste)
+document.write(teste2)

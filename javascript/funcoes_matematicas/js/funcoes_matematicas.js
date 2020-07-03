684f7fcd1605d2f057d8f94d1c0a4b29052aa925
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-07-03 18:28:57 -0300

    aula 180

---
+++ javascript/funcoes_matematicas/js/funcoes_matematicas.js
@@ -0,0 +1,8 @@
+var x  = 10.74
+
+document.write(Math.ceil(x),"<br>")
+document.write(Math.floor(x),"<br>")
+document.write(Math.round(x),"<br>")
+document.write((100*Math.random()).toFixed(2)+"%","<br>")
+document.write("D20: " +Math.round((20*Math.random())),"<br>")
+
